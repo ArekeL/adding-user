eb96e8df6eef9dfc741caff9fa1e49915775ba7b
Arek Lechnik <alechnik@gmail.com>
2023-03-17 06:22:29 +0100

    Adding css

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
+
 const AddUser = () => {
 	const handleAddUser = (event) => {
 		event.preventDefault();
 	};
 
 	return (
-		<form onSubmit={handleAddUser}>
-			<label htmlFor='userName'>User Name</label>
-			<input className='userName' type='text' />
-			<label htmlFor='userAge'>Age (Yers)</label>
-			<input className='userAge' type='number' />
-			<button>Add User</button>
-		</form>
+		<Card className={styles.input}>
+			<form onSubmit={handleAddUser}>
+				<label htmlFor='userName'>User Name</label>
+				<input type='text' />
+				<label htmlFor='userAge'>Age (Yers)</label>
+				<input type='number' />
+				<button>Add User</button>
+			</form>
+		</Card>
 	);
 };
 
